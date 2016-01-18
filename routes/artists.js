4c0d15966a78f0ee67da42decc67044eaa19faae
a25patel <aanaletap2510@gmail.com>
2016-01-18 08:02:27 -0700

    new page complete, figured out what bugs i want

--- routes/artists.js
+++ routes/artists.js
@@ -1,3 +1,7 @@
+// BUGS
+// 1. req,res,next -> res,req,next for one of the routes
+// 2. change order of something in app.js
+// 3. something with database or knex
 var express = require('express');
 var router = express.Router();
 
@@ -6,12 +10,34 @@ router.get('/', function(req, res, next) {
   res.render('artists/index');
 });
 
-// GET new form 
+// GET new form
+router.get('/new', function(req,res,next){
+  res.render('artists/new');
+});
+
 // POST new info
+router.post('', function(req,res,next){
+  res.redirect('');
+});
 
 //GET one listing
+router.get('', function(req,res,next){
+  res.render('');
+});
+
 //GET edit form
+router.get('', function(req,res,next){
+  res.render('');
+});
+
 //POST edits
+router.post('', function(req,res, next){
+  res.redirect('');
+});
+
 //POST delete listing
+router.post('', function(req,res,next){
+
+});
 
 module.exports = router;
