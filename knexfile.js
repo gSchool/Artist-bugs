16717177da4042578c7011793d658d050da3d550
a25patel <aanaletap2510@gmail.com>
2016-01-18 13:02:07 -0700

    initial commit

---
+++ knexfile.js
@@ -0,0 +1,11 @@
+// Update with your config settings.
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/art'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+  }
+};
---
+++ migrations/20160118111318_create_artists.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('artists', function(table){
+    table.increments();
+    table.string('name');
+    table.string('painting');
+    table.string('medium');
+    table.text('description');
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('artists');
+};
