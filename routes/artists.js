b93195532b1407b9ca876a8a718ddda63d24ea9c
Rachel Parri <erachelparri@gmail.com>
2016-01-19 07:58:26 -0700

    retrieved show page

--- routes/artists.js
+++ routes/artists.js
@@ -23,34 +23,33 @@ router.post('/artists', function(req,res,next){
   });
 });
 
-router.get('/artists/:id', function(res,req,next){
-  var artist_id = parseInt(req.params.id);
-  artists().where('id', artist_id).first().then(function(artist){
+router.get('/artists/:id', function(req,res,next){
+  artists().where('id', req.params.id).first().then(function(artist){
     res.render('artists/show', {artist: artist} );
   });
 });
 
-
-router.get('/artists/:id/edit', function(req, res, next) {
-  var artist_id = parseInt(req.params.id);
-  artists().where('id', artist_id).first().then(function(artist) {
-    res.render('artists/edit', {artist: artist});
-  });
-});
-
-router.post('/artists/:id', function (req, res, next) {
-  var artist_id = parseInt(req.params.id);
-  artists().where('id', artist_id).update(req.body).then(function(artist) {
-    res.redirect('/artists');
-  });
-});
-
-router.post('/artists/:id/delete', function (req, res, next) {
-  var artist_id = parseInt(req.params.id);
-  artists().where('id', artist_id).del().then(function (artist) {
-    res.redirect('/artists');
-  });
-});
+//
+// router.get('/artists/:id/edit', function(req, res, next) {
+//   var artist_id = parseInt(req.params.id);
+//   artists().where('id', artist_id).first().then(function(artist) {
+//     res.render('artists/edit', {artist: artist});
+//   });
+// });
+//
+// router.post('/artists/:id', function (req, res, next) {
+//   var artist_id = parseInt(req.params.id);
+//   artists().where('id', artist_id).update(req.body).then(function(artist) {
+//     res.redirect('/artists');
+//   });
+// });
+//
+// router.post('/artists/:id/delete', function (req, res, next) {
+//   var artist_id = parseInt(req.params.id);
+//   artists().where('id', artist_id).del().then(function (artist) {
+//     res.redirect('/artists');
+//   });
+// });
 
 
 module.exports = router;
