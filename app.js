91a77636fcd4cd633b856401b7fd29f08373d138
Sean Brage <seanmbrage@me.com>
2016-01-19 08:43:49 -0700

    For real, I cannot thank you enough. I really did not want to get anything else accomplished today besides getting node to properly excute bin/www at startup. All the kale and kombucha you consume must be making you psychic.

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/', routes);
-app.use('/artists', artists);
+app.use('/', artists);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
