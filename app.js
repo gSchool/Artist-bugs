1497b4ad60eaf15783ece0b14bad64b08eb2840a
Sean Brage <seanmbrage@me.com>
2016-01-19 08:33:20 -0700

    Ok, I\m

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
 var artists = require('./routes/artists');
 
@@ -19,10 +18,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/', routes);
-app.use('/', artists);
+app.use('/artists', artists);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
