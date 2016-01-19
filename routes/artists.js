ad23791d2a39e1fa67934c2f208b24a62a7b36d1
Rachel Parri <erachelparri@gmail.com>
2016-01-19 08:40:23 -0700

    fixed artists/index page to pull in proper values

--- routes/artists.js
+++ routes/artists.js
@@ -13,44 +13,44 @@ router.get('/artists', function(req,res,next){
   });
 });
 
-router.get('/artists/new', function(req,res,next){
-  res.render('artists/new');
-});
-
-router.post('/artists', function(req,res,next){
-  artists().insert(req.body).then(function(result){
-    res.redirect('/artists');
-  });
-});
-
-router.get('/artists/:id', function(res,req,next){
-  var artist_id = parseInt(req.params.id);
-  artists().where('id', artist_id).first().then(function(artist){
-    res.render('artists/show', {artist: artist} );
-  });
-});
-
-
-router.get('/artists/:id/edit', function(req, res, next) {
-  var artist_id = parseInt(req.params.id);
-  artists().where('id', artist_id).first().then(function(artist) {
-    res.render('artists/edit', {artist: artist});
-  });
-});
-
-router.post('/artists/:id', function (req, res, next) {
-  var artist_id = parseInt(req.params.id);
-  artists().where('id', artist_id).update(req.body).then(function(artist) {
-    res.redirect('/artists');
-  });
-});
-
-router.post('/artists/:id/delete', function (req, res, next) {
-  var artist_id = parseInt(req.params.id);
-  artists().where('id', artist_id).del().then(function (artist) {
-    res.redirect('/artists');
-  });
-});
-
+// router.get('/artists/new', function(req,res,next){
+//   res.render('artists/new');
+// });
+//
+// router.post('/artists', function(req,res,next){
+//   artists().insert(req.body).then(function(result){
+//     res.redirect('/artists');
+//   });
+// });
+//
+// router.get('/artists/:id', function(res,req,next){
+//   var artist_id = parseInt(req.params.id);
+//   artists().where('id', artist_id).first().then(function(artist){
+//     res.render('artists/show', {artist: artist} );
+//   });
+// });
+//
+//
+// router.get('/artists/:id/edit', function(req, res, next) {
+//   var artist_id = parseInt(req.params.id);
+//   artists().where('id', artist_id).first().then(function(artist) {
+//     res.render('artists/edit', {artist: artist});
+//   });
+// });
+//
+// router.post('/artists/:id', function (req, res, next) {
+//   var artist_id = parseInt(req.params.id);
+//   artists().where('id', artist_id).update(req.body).then(function(artist) {
+//     res.redirect('/artists');
+//   });
+// });
+//
+// router.post('/artists/:id/delete', function (req, res, next) {
+//   var artist_id = parseInt(req.params.id);
+//   artists().where('id', artist_id).del().then(function (artist) {
+//     res.redirect('/artists');
+//   });
+// });
+//
 
 module.exports = router;
